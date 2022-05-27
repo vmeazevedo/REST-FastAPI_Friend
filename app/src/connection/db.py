a7589d12bcfb3ace7ebc520dc740236416279055
Extremis@22 <vmeazevedo@gmail.com>
2022-05-27 15:30:06 -0300

    FIX: refatoração dos comentários do db

--- app/src/connection/db.py
+++ app/src/connection/db.py
@@ -2,13 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#define sqlite connection url
+# Definindo a conexão com o banco de dados
 SQLALCHEMY_DATABASE_URL = "sqlite:///./database/friends_api.db"
 
-# create new engine instance 
+# Criando a conexão com o banco de dados
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-# create sessionmaker 
+# Criando o Sessionmaker
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
